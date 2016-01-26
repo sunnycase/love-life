d8761b28316b4e8ec8fa79a21d8e01d6dbeb9ed0
sunnycase <sunnycase@live.cn>
2016-01-26 18:41:21 +0800

    添加主页

---
+++ Tomato.Lovelife/ShellModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Tomato.Lovelife.ViewModels;
+using Windows.ApplicationModel.Resources;
+
+namespace Tomato.Lovelife
+{
+    static class ShellModule
+    {
+        public static void UseShell(this SimpleContainer container)
+        {
+            container.PerRequest<MainViewModel>();
+            container.Instance(ResourceLoader.GetForCurrentView());
+        }
+
+        private static void ModuleReferences()
+        {
+            var type = new[]
+            {
+                typeof(Caliburn.Micro.ViewModelBinder)
+            };
+        }
+    }
+}
---
+++ Tomato.Lovelife/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tomato.Lovelife.Views;
+using Tomato.Uwp.Mvvm;
+using Windows.UI.Xaml.Controls;
+
+namespace Tomato.Lovelife.ViewModels
+{
+    class MainViewModel : BindableBase
+    {
+        private Frame _navigationService;
+
+        public void SetupNavigationService(object sender, object e)
+        {
+            _navigationService = (Frame)sender;
+            NavigateToHome();
+        }
+
+        public void NavigateToHome()
+        {
+            _navigationService?.Navigate(typeof(HomeView));
+        }
+
+        public void NavigateToSettings()
+        {
+
+        }
+    }
+}
